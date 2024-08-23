c4d235a5465f4a8e46e74c945fb66d99136a206b
Ivan Dachev <i_dachev@yahoo.co.uk>
2024-08-23 13:22:34 +0300

    Revert "Bump version to 0.1.4"
    This reverts commit 155f9e73f186f3c56cf6d36b08a1b6b51d4d8718.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='openapi-trimmer',
-        version='0.1.4',
+        version='0.1.3',
         packages=['openapi_trimmer'],
         include_package_data=True,
         install_requires=[
